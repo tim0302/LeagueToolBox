57cd7cad605126757c36251ed3894d0841a14fca
102419310 <54136440+tim0302@users.noreply.github.com>
2022-08-03 20:03:45 +1000

    Filtering champions based on user choice

--- leaguetoolbox/src/App.js
+++ leaguetoolbox/src/App.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 function App() {
-  const [chosenRank, SetChosenRank] = useState(null);
+  const [chosenRank, setChosenRank] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [championList, setChampionList] = useState(null);
-  function ShowVariable() {
-    console.log(championList);
+  const [filteredChampionList, setFilteredChampionList] = useState(null);
+  const [skillLevel, setSkillLevel] = useState(null);
+  const [role, setRole] = useState(null);
+
+  function filterChampionBaseOnSkill() {
+    console.log(championList.name);
   }
-  function GetAllChampions() {
+  function getAllChampions() {
     setIsLoading(true);
 
     const options = {
@@ -33,37 +37,83 @@ function App() {
   }
 
   useEffect(() => {
-    if (chosenRank) {
-      GetAllChampions();
+    if (chosenRank && skillLevel && role) {
+      getAllChampions();
     }
     //run when ever this changes
   }, [chosenRank]);
   return (
     <div className='App'>
-      {!chosenRank && (
-        <div className='Rank-selector'>
-          <h1>League of Legends Champion Picker</h1>
-          <p>Select Your Rank to start</p>
-          <select
-            name='ranks'
-            id='ranks'
-            value={chosenRank}
-            onChange={(e) => SetChosenRank(e.target.value)}
-          >
-            <option value='rank'>Choose a Rank</option>
+      {/* {!chosenRank && !skillLevel && !role && ( */}
+      <div className='Rank-selector'>
+        <h1>League of Legends Champion Picker</h1>
+        <p>Select Your Rank & skill Level to start</p>
+        <select
+          name='ranks'
+          id='ranks'
+          value={chosenRank}
+          onChange={(e) => setChosenRank(e.target.value)}
+        >
+          <option value={null}>Choose your Rank</option>
+          <option value='iron'>Iron</option>
+          <option value='bronze'>Bronze</option>
+          <option value='silver'>Silver</option>
+          <option value='gold'>Gold</option>
+          <option value='platinum'>Platinum</option>
+          <option value='diamond'>Diamond</option>
+          <option value='master'>Master</option>
+          <option value='grandmaster'>Grandmaster</option>
+          <option value='challenger'>Challenger</option>
+        </select>
 
-            <option value='Iron'>Iron</option>
-            <option value='Bronze'>Bronze</option>
-            <option value='Silver'>Silver</option>
-          </select>
+        <select
+          name='skill'
+          id='skill'
+          value={skillLevel}
+          onChange={(e) => setSkillLevel(e.target.value)}
+        >
+          <option value={null}>Choose your skill level</option>
+
+          <option value='D'>No one can beat me in this game.</option>
+          <option value='S'>I'm new!</option>
+        </select>
+
+        <select
+          name='role'
+          id='role'
+          value={role}
+          onChange={(e) => setRole(e.target.value)}
+        >
+          <option value={null}>Choose your prefered role</option>
+
+          <option value='TOP'>Top</option>
+          <option value='MID'>Mid</option>
+          <option value='JUNGLE'>Jungle</option>
+          <option value='ADC'>ADC</option>
+          <option value='SUPPORT'>Support</option>
+        </select>
+      </div>
+      {/* )} */}
+      {isLoading && <h2>Loading...</h2>}
+      {/* {!championList && <h2>Loading the variable...</h2>} */}
+      {chosenRank && skillLevel && role && (
+        <button onClick={() => filterChampionBaseOnSkill()}>
+          Click to load the champions
+        </button>
+      )}
+      {championList && chosenRank && skillLevel && role && (
+        <div className='output'>
+          <h5>Introducing your champions:</h5>
+          {championList.map((champion, _championIndex) =>
+            champion.tier.includes(skillLevel) && champion.role === role ? (
+              <p>
+                {champion.name} - win rate: {champion.winRate}
+              </p>
+            ) : null
+          )}
         </div>
       )}
-      {isLoading && <h2>Loading...</h2>}
-      {!championList && <h2>Loading the variable...</h2>}
-
-      <button>Click to load the champions</button>
-
-      <button onClick={() => ShowVariable()}>ShowVariable</button>
+      {/* <button onClick={() => ShowVariable()}>ShowVariable</button> */}
     </div>
   );
 }
